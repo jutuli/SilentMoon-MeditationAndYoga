2c1bbf54e82039db231088ba98545cceb29cc0f3
Julia T <juliaturkali@gmail.com>
2025-04-28 23:24:52 +0200

    refactor: update CategoryFilter to use ICategory interface and show FA icons

--- src/components/CategoryFilter.tsx
+++ src/components/CategoryFilter.tsx
@@ -1,35 +1,33 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import * as SolidIcons from "@fortawesome/free-solid-svg-icons";
 import { useEffect, useState } from "react";
 import supabase from "../utils/supabase";
-import { ICategory } from "../pages/Yoga";
+import { ICategory } from "../interfaces/ICategory";
 
 interface ICategoryFilterProps {
   //! hard gecoded, weil sonst weiterer fetch?!-so auch okay?
-  type: "431a7ad8-a08b-429e-820c-24c53c240990" | "8d946553-91d1-4307-915a-a2b5329769e2";
+  type:
+    | "431a7ad8-a08b-429e-820c-24c53c240990"
+    | "8d946553-91d1-4307-915a-a2b5329769e2";
 }
-
-
 
 const CategoryFilter = ({ type }: ICategoryFilterProps) => {
-
-  const [categories, setCategories] = useState<ICategory[] | undefined>()
+  const [categories, setCategories] = useState<ICategory[] | undefined>();
 
   const fetchCategories = async () => {
-    const {data, error} = await supabase
-    .from("categories")
-    .select("*")
+    const { data, error } = await supabase.from("categories").select("*");
 
     if (error) {
-        console.log(error);
+      console.log(error);
     } else {
-        setCategories(data);
-        console.log(data);
+      setCategories(data);
+      console.log(data);
     }
-}
+  };
 
-useEffect(()=> {
-    fetchCategories()
-}, [])
+  useEffect(() => {
+    fetchCategories();
+  }, []);
 
   // später durch supabase ersetzen
   // const categorie = [
@@ -56,40 +54,41 @@ useEffect(()=> {
   // );
   const [activeCategory, setActiveCategory] = useState<string | null>(null);
 
-
-
-  const handleClick = (categorie : ICategory) => {
+  const handleClick = (categorie: ICategory) => {
     setActiveCategory(categorie.id);
   };
 
   return (
     <div className="category-filter w-full bg-white px-4 py-2">
       <ul className="carousel flex justify-between pb-5">
-        {categories?.map((category) => {
+        {categories?.map((category: ICategory) => {
           const isActive = activeCategory === category.id;
+          const icon = (SolidIcons as any)[category.icon];
           return (
             <div className="carousel-item">
               <li
-              key={category.id}
-              onClick={() => handleClick(category)}
-              className="flex cursor-pointer flex-col items-center text-sm"
-            >
-              <div
-                className={`flex aspect-square w-15 items-center justify-center rounded-2xl ${
-                  isActive ? "bg-light-green text-cream" : "bg-gray text-white"
-                }`}
+                key={category.id}
+                onClick={() => handleClick(category)}
+                className="flex cursor-pointer flex-col items-center text-sm"
               >
-                {/* Funktioniert nicht */}
-                <FontAwesomeIcon icon={category.icon} className="text-xl" />
-              </div>
-              <p
-                className={`text-xs w-15 text-center mt-1 ${
-                  isActive ? "text-light-green" : "text-gray"
-                }`}
-              >
-                {category.name}
-              </p>
-            </li>
+                <div
+                  className={`flex aspect-square w-15 items-center justify-center rounded-2xl ${
+                    isActive
+                      ? "bg-light-green text-cream"
+                      : "bg-gray text-white"
+                  }`}
+                >
+                  {/* Funktioniert nicht */}
+                  <FontAwesomeIcon icon={icon} className="text-xl" />
+                </div>
+                <p
+                  className={`mt-1 w-15 text-center text-xs ${
+                    isActive ? "text-light-green" : "text-gray"
+                  }`}
+                >
+                  {category.name}
+                </p>
+              </li>
             </div>
           );
         })}
---
+++ src/interfaces/ICategory.ts
@@ -0,0 +1,7 @@
+export interface ICategory {
+  id: string;
+  name: string;
+  type_id: string;
+  icon: string;
+  created_at: string;
+}
